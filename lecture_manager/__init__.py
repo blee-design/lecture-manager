200cb20b60851c15190a844d432ae5270521b142
Udaya Raj Joshi <udayarajjoshi@aol.com>
2026-07-08 14:25:54 +0545

    Bump version to 2.3.3

--- lecture_manager/__init__.py
+++ lecture_manager/__init__.py
@@ -6,4 +6,4 @@ YouTube Lecture Manager package.
 
 from .facebook_manager import add_facebook_entry, get_facebook_entry_by_id, list_facebook_entries, delete_facebook_entry
 
-__version__ = "2.3.2"
+__version__ = "2.3.3"
