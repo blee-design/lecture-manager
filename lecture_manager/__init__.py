6aab4b59aef40888337d0db199a3581061840670
Udaya Raj Joshi <udayarajjoshi@aol.com>
2026-07-20 12:17:55 +0545

    Fixed upload version

--- lecture_manager/__init__.py
+++ lecture_manager/__init__.py
@@ -6,4 +6,4 @@ YouTube Lecture Manager package.
 
 from .facebook_manager import add_facebook_entry, get_facebook_entry_by_id, list_facebook_entries, delete_facebook_entry
 
-__version__ = "2.4.0-1"
+__version__ = "2.4.0-3"
